d628f893908c7b81932a6624e06f2247c6ff7d34
Stnislav-Kankin <stanislav.kankin@yandex.ru>
2024-10-14 19:26:40 +0300

    time

--- src/__main__.py
+++ src/__main__.py
@@ -1,5 +1,6 @@
 from random import randint
 from datetime import datetime, timedelta
+from zoneinfo import ZoneInfo
 from typing import Callable, Awaitable, Any
 
 from aiogram import Bot, Dispatcher, BaseMiddleware
@@ -74,7 +75,7 @@ async def start(message: Message, user: User):
     next_usage = user.next_usage and f"{user.next_usage:%c}"
 
     markup = None
-    if not next_usage or (datetime.utcnow() + timedelta(hours=5, seconds=30)) < tz.make_naive(user.next_usage):
+    if not next_usage or (datetime.now(ZoneInfo("Europe/Moscow")) + timedelta(seconds=30)) < tz.make_naive(user.next_usage):
         markup = (
             InlineKeyboardBuilder()
             .button(text="🍀 Испытай свою удачу!", web_app=WebAppInfo(url=config.WEBAPP_URL))
@@ -101,8 +102,8 @@ async def open_box(request: Request):
     except ValueError:
         return JSONResponse({"success": False, "error": "Unauthorized"}, 401)
 
-    current_datetime = datetime.utcnow() + timedelta(hours=5, seconds=30)
-    add_1h = current_datetime + timedelta(hours=5, seconds=30)
+    current_datetime = datetime.now(ZoneInfo("Europe/Moscow"))
+    add_1h = current_datetime + timedelta(seconds=30)
 
     cash = randint(0, 1000)
     user = await User.filter(id=data.user.id).first()
