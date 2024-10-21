4c113176dbd26c36f6b5c91453adefcf6fb0679a
Stnislav-Kankin <stanislav.kankin@yandex.ru>
2024-10-21 11:34:28 +0300

    больше документации богу документации

--- src/config_reader.py
+++ src/config_reader.py
@@ -1,25 +1,30 @@
 import os
-
 from pydantic import SecretStr
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
+# Получаем текущую директорию, где находится этот файл
 current_dir = os.path.dirname(os.path.abspath(__file__))
 
 
 class Config(BaseSettings):
-    BOT_TOKEN: SecretStr
-    DB_URL: SecretStr
+    """
+    Класс конфигурации для хранения настроек приложения.
+    """
 
-    WEBAPP_URL: str = "https://df04-147-45-193-130.ngrok-free.app"  #ТУННЕЛЬ
+    BOT_TOKEN: SecretStr  # Токен бота
+    DB_URL: SecretStr  # URL базы данных
 
-    CURRENT_DIR: str = os.path.dirname(os.path.abspath(__file__))
-    TEMPLATES_PATH: str = os.path.join(current_dir, "web", "templates")
-    STATIC_PATH: str = os.path.join(current_dir, "web", "static")
+    WEBAPP_URL: str = "https://df04-147-45-193-130.ngrok-free.app"  # URL веб-приложения (туннель)
+
+    CURRENT_DIR: str = os.path.dirname(os.path.abspath(__file__))  # Текущая директория
+    TEMPLATES_PATH: str = os.path.join(current_dir, "web", "templates")  # Путь к шаблонам
+    STATIC_PATH: str = os.path.join(current_dir, "web", "static")  # Путь к статическим файлам
 
     model_config = SettingsConfigDict(
-        env_file=os.path.join(current_dir, ".env"),
-        env_file_encoding="utf-8"
+        env_file=os.path.join(current_dir, ".env"),  # Путь к файлу с переменными окружения
+        env_file_encoding="utf-8"  # Кодировка файла с переменными окружения
     )
 
 
+# Создаем экземпляр класса Config для использования в приложении
 config = Config()
